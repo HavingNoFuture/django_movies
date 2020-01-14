000701ea1a70b44e73a20af484dfaafb5bf756b1
Alexey <alex.erm@yandex.ru>
2020-01-14 18:18:03 +0300

    Добавил шаблон деталей фильма, урл и отображение.

--- movie_catalog/urls.py
+++ movie_catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from movie_catalog import views
 
 urlpatterns = [
-    path('catalog/', views.MovieView.as_view(), name="movie_catalog"),
+    path('catalog/', views.MovieListView.as_view(), name="movie_list"),
+    path('<int:pk>', views.MovieDetailView.as_view(), name="movie_detail"),
 ]
--- movie_catalog/views.py
+++ movie_catalog/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MovieView(View):
+class MovieListView(View):
     """Список фильмов"""
-
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movie_catalog/movie_list.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """Описание фильма"""
+    def get(self, request, pk):
+        movie = get_object_or_404(Movie, id=pk)
+        return render(request, "movie_catalog/movie_detail.html", {"movie": movie})
+
