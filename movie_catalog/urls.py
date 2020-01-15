560ef7b03a6aa691c63343c7afbc3683bb917228
Alexey <alex.erm@yandex.ru>
2020-01-15 20:50:21 +0300

    Отркфакторил шаблоны

--- movie_catalog/urls.py
+++ movie_catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from movie_catalog import views
 
 urlpatterns = [
-    path('catalog/', views.MovieListView.as_view(), name="movie_list"),
+    path('', views.MovieListView.as_view(), name="movie_list"),
     path('<str:slug>', views.MovieDetailView.as_view(), name="movie_detail"),
 ]
